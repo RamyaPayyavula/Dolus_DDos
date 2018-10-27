e9bec69ae5bd9705b82cbb2e71e2a48bb937280b
Ramya Payyavula <ramyaram48@gmail.com>
2018-10-27 17:28:04 -0500

    Update calcSS.py

--- app/Python/calcSS.py
+++ app/Python/calcSS.py
@@ -3,12 +3,12 @@ import MySQLdb
 import MySQLdb.cursors
 import time
 timestamp = time.time()
-current_date_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+current_date_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
 def calculateSS(trace_id,device_id):
     db = MySQLdb.connect(user='root', passwd='root', host='127.0.0.1', db='test', cursorclass=MySQLdb.cursors.DictCursor)
     cursor = db.cursor()
-    query = "INSERT INTO suspiciousness_scores SELECT "+ str(device_id)+","+str(trace_id)+","+str(current_date_timestamp)+",g.name, g.score"
+    query = "INSERT INTO suspiciousness_scores SELECT "+ str(device_id)+","+str(trace_id)+",'"+str(current_date_timestamp)+"',g.name, g.score"
     query += " FROM (SELECT a.name, a.bytes_total, a.bytes_min, a.bytes_max, a.flows_total,"
     query += "a.flows_min, a.flows_max, a.connections_total, a.connections_min, a.connections_max,"
     query += "(connections_total - connections_min) / (connections_max - connections_min)"
