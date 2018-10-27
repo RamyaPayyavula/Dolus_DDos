4e63ff26884cfb2ae918e16aae89ac318c5e80be
Ramya Payyavula <ramyaram48@gmail.com>
2018-10-27 15:38:43 -0500

    sixth commit update of SS scores

--- app/Python/calcSS.py
+++ app/Python/calcSS.py
@@ -1,40 +1,31 @@
-import sys
-from settings import Session, engine, Base
 import datetime
+import MySQLdb
+import MySQLdb.cursors
 import time
 timestamp = time.time()
-current_date_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+current_date_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
 
-def calculateSS(trace_id):
-    session = Session()
-    query = f""" INSERT INTO suspiciousness_scores(traceID,ssscore_caluculated_time,device_id,score) (SELECT {str(trace_id)}, '{str(current_date_timestamp)}',g.deviceID,g.score
-                 FROM (SELECT a.deviceID, c.bytes_total, c.bytes_min, c.bytes_max, b.flows_total,
-                 b.flows_min, b.flows_max, a.connections_total, a.connections_min, a.connections_max,
-                 (connections_total - connections_min) / (connections_max - connections_min)
-                 as 'connections_normalized',(flows_total - flows_min) / (flows_max - flows_min)
-                 as 'flows_normalized',(bytes_total - bytes_min) / (bytes_max - bytes_min) as 'bytes_normalized',
-                 POWER(((POWER(((connections_total - connections_min) / (connections_max - connections_min)),2)
-                 + POWER(((flows_total - flows_min) / (flows_max - flows_min)),2)
-                 +POWER(((bytes_total - bytes_min) / (bytes_max - bytes_min)),2))/3),(1.0/2.0)) 'score'
-                 from (select d.name,d.deviceID, COUNT(DISTINCT ip_dst) AS 'connections_total',
-                 case when d.name like 'server%' then 10 else 1 end as 'connections_min',
-                 case when d.name like 'server%' then 1000 else 10 end as 'connections_max'
-                 FROM packet_logs l, devices d WHERE d.ipv4 = l.ip_src and ip_src <> ' '
-                 and ip_dst <> ' ' and trace_id = {str(trace_id)} group by d.name) a,
-                 (SELECT d.name, count(*) as 'flows_total', case when d.name like 'server%' then 1000 else 100
-                 end as 'flows_min', case when d.name like 'server%' then 10000 else 1000 end as 'flows_max'
-                 FROM packet_logs l, devices d WHERE d.ipv4 = l.ip_src and ip_src <> ' '
-                 and ip_dst <> ' ' and trace_id = {str(trace_id)} group by d.name) b,
-                 (SELECT d.name , SUM(frame_len) as 'bytes_total',
-                 case when d.name like 'server%' then 100000 else 100 end as 'bytes_min',
-                 case when d.name like 'server%' then 100000000 else 100000 end as 'bytes_max'
-                 FROM packet_logs l, devices d WHERE d.ipv4 = l.ip_src and ip_src <> ' '
-                 and ip_dst <> ' ' and (trace_id = {str(trace_id)})  group by d.name) c
-                 WHERE a.name = b.name and a.name = c.name)g);"""
+def calculateSS(trace_id,device_id):
+    db = MySQLdb.connect(user='root', passwd='root', host='127.0.0.1', db='test', cursorclass=MySQLdb.cursors.DictCursor)
+    cursor = db.cursor()
+    query = "INSERT INTO suspiciousness_scores SELECT "+ str(device_id)+","+str(trace_id)+","+str(current_date_timestamp)+",g.name, g.score"
+    query += " FROM (SELECT a.name, a.bytes_total, a.bytes_min, a.bytes_max, a.flows_total,"
+    query += "a.flows_min, a.flows_max, a.connections_total, a.connections_min, a.connections_max,"
+    query += "(connections_total - connections_min) / (connections_max - connections_min)"
+    query += "as 'connections_normalized',(flows_total - flows_min) / (flows_max - flows_min)"
+    query += "as 'flows_normalized',(bytes_total - bytes_min) / (bytes_max - bytes_min) as 'bytes_normalized',"
+    query += "POWER(((POWER(((connections_total - connections_min) / (connections_max - connections_min)),2)"
+    query += "+ POWER(((flows_total - flows_min) / (flows_max - flows_min)),2)"
+    query += "+POWER(((bytes_total - bytes_min) / (bytes_max - bytes_min)),2))/3),(1.0/2.0)) 'score'"
+    query += " from("
+    query += "SELECT d.name, SUM(l.frame_len) as 'bytes_total', case when d.name like 'server%' then 100000 else 100 end as 'bytes_min',"
+    query += "case when d.name like 'server%' then 100000000 else 100000 end as 'bytes_max',"
+    query += "count(*) as 'flows_total',case when d.name like 'server%' then 1000 else 100 end as 'flows_min',"
+    query += "case when d.name like 'server%' then 10000 else 1000 end as 'flows_max',"
+    query += "COUNT(DISTINCT l.ip_dst) AS 'connections_total',case when d.name like 'server%' then 10 else 1 end as 'connections_min',"
+    query += "case when d.name like 'server%' then 1000 else 10 end as 'connections_max'"
+    query += "FROM packet_logs l, devices d WHERE d.ipv4 = l.ip_src and l.ip_src <> ' '"
+    query += "and l.ip_dst <> ' ' and trace_id = "+ str(trace_id) +" group by d.name) a	) g;"
 
-
-
-    # print('query', query)
-    session.execute(query)
-    session.commit()
-
+    cursor.execute(query)
+    db.commit()
