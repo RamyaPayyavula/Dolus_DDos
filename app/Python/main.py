debf6fc12bcc8250eaac5e54210e623d47bae969
Ramya Payyavula <ramyaram48@gmail.com>
2018-11-01 19:48:01 -0500

    Update main.py

--- app/Python/main.py
+++ app/Python/main.py
@@ -6,7 +6,7 @@ import sys
 from app.Python.settings import Session, engine, Base
 from app.Python.calcSSByTime import calculateSSByTime
 from app.Python.calcSS import calculateSS
-from app.Python.models import SuspiciousnessScores
+from app.Python.models import SuspiciousnessScores, devices
 
 session = Session()
 lastrecord = session.query(SuspiciousnessScores).order_by(SuspiciousnessScores.traceID.desc()).first()
@@ -15,10 +15,12 @@ if lastrecord is None:
     trace_id = 1
 else:
     trace_id = lastrecord.traceID +1
-calculateSS(trace_id)
-records = session.query(SuspiciousnessScores).all()
+
+records = session.query(Devices).all()
+row_count = session.query(SuspiciousnessScores).count()
 for rec in records:
-    trace_id1 = rec.traceID
     device_id = rec.deviceID
-    calculateSSByTime(trace_id, device_id)
+    calculateSS(row_count,trace_id,device_id)
+    calculateSSByTime(row_count,trace_id, device_id)
+    row_count = row_count+1
 print('executed')
