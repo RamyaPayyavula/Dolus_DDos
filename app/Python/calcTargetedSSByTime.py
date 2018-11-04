4b2c5268e8f969f1a96e7428cbb5bf842f883a82
Ramya Payyavula <ramyaram48@gmail.com>
2018-11-03 22:02:45 -0500

    Update calcTargetedSSByTime.py

--- app/Python/calcTargetedSSByTime.py
+++ app/Python/calcTargetedSSByTime.py
@@ -2,14 +2,16 @@ import datetime
 import MySQLdb
 import MySQLdb.cursors
 import time
+from settings import Session, engine, Base
+from models import SuspiciousnessScoresByTime
 
 timestamp = time.time()
 current_date_timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
+session = Session()
 
-def calculateTargetedSSByTime(SSSID,trace_id, device_id):
-    db = MySQLdb.connect(user='root', passwd='root', host='127.0.0.1', db='test',
-                         cursorclass=MySQLdb.cursors.DictCursor)
+def calculateTargetedSSByTime(trace_id, device_id):
+    db = MySQLdb.connect(user='root', passwd='root', host='127.0.0.1', db='test',cursorclass=MySQLdb.cursors.DictCursor)
     cursor = db.cursor()
     findMin = "SELECT MIN(frame_time) as minFrameTime, MAX(frame_time) as maxFrameTime FROM packet_logs where trace_id = " + str(trace_id) + ";"
     cursor.execute(findMin)
@@ -17,12 +19,14 @@ def calculateTargetedSSByTime(SSSID,trace_id, device_id):
 
     minFrame = results["minFrameTime"]
     maxFrame = results["maxFrameTime"]
+    SSSID = session.query(SuspiciousnessScoresByTime).count()
     try:
         while minFrame < maxFrame:
             db = MySQLdb.connect(user='root', passwd='root', host='127.0.0.1', db='test',
                                  cursorclass=MySQLdb.cursors.DictCursor)
             cursor = db.cursor()
-            query = "INSERT INTO suspiciousness_scores SELECT "+ str(SSSID)+","+ str(device_id) + "," + str(trace_id) + ",'"+str(current_date_timestamp)+"',g.name, g.score"
+            SSSID = SSSID+1;
+            query = "INSERT INTO suspiciousness_scores_by_time SELECT "+ str(SSSID)+","+ str(device_id) + "," + str(trace_id) + ",'"+str(current_date_timestamp)+"',g.name, g.score"
             query += " FROM (SELECT a.name, a.bytes_total, a.bytes_min, a.bytes_max, a.flows_total,"
             query += "a.flows_min, a.flows_max, a.connections_total, a.connections_min, a.connections_max,"
             query += "(connections_total - connections_min) / (connections_max - connections_min)"
@@ -32,14 +36,14 @@ def calculateTargetedSSByTime(SSSID,trace_id, device_id):
             query += "+ POWER(0.7*((flows_total - flows_min) / (flows_max - flows_min)),2)"
             query += "+POWER(0.7*((bytes_total - bytes_min) / (bytes_max - bytes_min)),2))/3),(1.0/2.0)) 'score'"
             query += " from("
-            query += "SELECT d.name, SUM(l.frame_len) as 'bytes_total', case when d.name like 'server%' then 100000 else 100 end as 'bytes_min',"
+            query += "SELECT d.name, CASE WHEN SUM(frame_len)<> NULL THEN SUM(frame_len) ELSE 0 END as 'bytes_total', case when d.name like 'server%' then 100000 else 100 end as 'bytes_min',"
             query += "case when d.name like 'server%' then 100000000 else 100000 end as 'bytes_max',"
             query += "count(*) as 'flows_total',case when d.name like 'server%' then 1000 else 100 end as 'flows_min',"
             query += "case when d.name like 'server%' then 10000 else 1000 end as 'flows_max',"
             query += "COUNT(DISTINCT l.ip_dst) AS 'connections_total',case when d.name like 'server%' then 10 else 1 end as 'connections_min',"
             query += "case when d.name like 'server%' then 1000 else 10 end as 'connections_max'"
-            query += "FROM packet_logs l, devices d WHERE d.ipv4 = l.ip_src and l.ip_src <> ' '"
-            query += "and l.ip_dst <> ' ' and trace_id = " + str(trace_id) + " and frame_time >= " + str(minFrame) + " and frame_time < " + str(minFrame) + " + 10 group by d.name) a	) g;"
+            query += "FROM packet_logs l, devices d WHERE d.deviceID="+str(device_id)+" and d.ipv4 = l.ip_src and l.ip_src <> ' '"
+            query += "and l.ip_dst <> ' ' and trace_id = " + str(trace_id) + " and frame_time >= " + str(minFrame) + " and frame_time < " + str(minFrame) + " + 10 ) a	) g;"
 
             # print(query)
             cursor.execute(query)
