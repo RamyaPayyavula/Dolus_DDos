36462fe4fca53c26cb6fef615886cf09406d504f
Ramya Payyavula <ramyaram48@gmail.com>
2018-11-06 00:29:39 -0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,41 @@
 $qvms = $info['qvms'];
 $servers = $info['servers'];
 $users = $info['users'];
+$rootswitch_logs = $info['rootswitch_logs'];
+$slaveswitch_logs = $info['slaveswitch_logs'];
+$totalnetwork_bandwidth = array();
+$root_switchdata = array();
+$slave_switchdata = array();
+if(count($rootswitch_logs)>0)
+{
+	
+	for($i=0;$i<count($rootswitch_logs);$i++)
+	{
+		$root_switchdata[$i][0] = strval($rootswitch_logs[$i]['unixtimestamp']);
+		$root_switchdata[$i][1] = ($rootswitch_logs[$i]['tx_bytes'] + $rootswitch_logs[$i]['rx_bytes'])*8;
+		$totalnetwork_bandwidth[$i][0] = strval($rootswitch_logs[$i]['unixtimestamp']);
+		$totalnetwork_bandwidth[$i][1] = ($rootswitch_logs[$i]['total_bytes'])*8;
+	}
+}
+
+if(count($slaveswitch_logs)>0)
+{
+
+
+	for($i=0;$i<count($slaveswitch_logs);$i++)
+	{
+		$slave_switchdata[$i][0] = strval($slaveswitch_logs[$i]['unixtimestamp']);
+		$slave_switchdata[$i][1] = ($slaveswitch_logs[$i]['tx_bytes'] + $slaveswitch_logs[$i]['rx_bytes'])*8;
+		if($totalnetwork_bandwidth[$i][1])
+		{
+	    $totalnetwork_bandwidth[$i][1] = ($totalnetwork_bandwidth[$i][1])+($slaveswitch_logs[$i]['total_bytes'])*8;
+		}
+		else
+		{
+			$totalnetwork_bandwidth[$i][1] = ($slaveswitch_logs[$i]['total_bytes'])*8;
+		}
+	}
+}
 
 ?>
 
@@ -10,13 +45,83 @@ $users = $info['users'];
 
 <title>MTD | Dashboard</title>
 @section('content')
+ <html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>MTD | Blacklist</title>
 
+        <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" />
+
+        <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" />
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<script type="text/javascript">
+            google.charts.load('current', {packages: ['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+            function drawChart() {
+                / Define the chart to be drawn.
+				var today = date("F j, Y");
+				var data1 = new google.visualization.DataTable();
+                data1.addColumn('string', 'Date - Time');
+                data1.addColumn('number', 'Bandwidth Utilization');
+                data1.addRows(<?php echo(json_encode($totalnetwork_bandwidth)) ?>);
+                var options1 = {'title':'Newtork bandwidth utilization on '.data,
+                               'orientation': 'horizontal',
+                               'width':'100%',
+                               'height':500};
+                var data2 = new google.visualization.DataTable();
+                data2.addColumn('string', 'Date - Time');
+                data2.addColumn('number', 'Bandwidth Utilization');
+                data2.addRows(<?php echo(json_encode($root_switchdata)) ?>);
+                var options2 = {'title':'root switch bandwidth utilization on '.data,
+                               'orientation': 'horizontal',
+                               'width':'100%',
+                               'height':500};
+			    var data3 = new google.visualization.DataTable();
+                data3.addColumn('string', 'Date - Time');
+                data3.addColumn('number', 'Bandwidth Utilization');
+                data3.addRows(<?php echo(json_encode($slave_switchdata)) ?>);
+                var options3 = {'title':'slave switch bandwidth utilization on '.data,
+                               'orientation': 'horizontal',
+                               'width':'100%',
+                               'height':500};
+                // Instantiate and draw the chart.
+				var chart1 = new google.visualization.LineChart(document.getElementById('totalNewtorkGraph'));
+                chart1.draw(data1, options1);
+                var chart2 = new google.visualization.LineChart(document.getElementById('rootSwitchGraph'));
+                chart2.draw(data2, options2);
+				var chart3 = new google.visualization.LineChart(document.getElementById('slaveSwitchGraph'));
+                chart3.draw(data3, options3);
+            }
+        </script>
+
+        <style>
+            .info-box-number{
+                font-size:32px;
+            }
+            span.label{
+                font-size:12px;
+            }
+            .flot-x-axis .flot-tick-label {
+                white-space: nowrap;
+                transform: translate(-9px, 0) rotate(-70deg);
+                text-indent: -100%;
+                transform-origin: top right;
+                text-align: right !important;
+            }
+
+        </style>
+
+
+    </head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
     <div class="hold-transition skin-blue sidebar-mini">
 
         <div class="wrapper">
 
             <!-- Content Wrapper. Contains page content -->
-        {{--<div class="content-wrapper">--}}
         <!-- Main content -->
             <section class="content">
 
@@ -84,17 +189,12 @@ $users = $info['users'];
                                 <div class="box-tools pull-right">
                                     <button type="button" onclick="location.reload();" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-refresh"></i>
                                     </button>
-                                <!-- <div class="box-tools pull-right">
-                                  Real time
-                                  <div class="btn-group" id="realtime" data-toggle="btn-toggle">
-                                    <button type="button" class="btn btn-default btn-xs active" data-toggle="on">On</button>
-                                    <button type="button" class="btn btn-default btn-xs" data-toggle="off">Off</button>
-                                  </div>
-                                </div> -->
+
                             </div>
                             <div class="box-body">
-                                <div id="overallBandwidth" style="height: 300px;"></div><!--interactive-->
-                            </div>
+								<div id="totalNewtorkGraph" style="height: 100%; width:100%"></div>
+                             </div>                            
+							 </div>
                             <!-- /.box-body-->
                         </div>
                         <!-- /.box -->
@@ -117,17 +217,11 @@ $users = $info['users'];
                                     </button>
                                 </div>
 
-                                <!-- <div class="box-tools pull-right">
-                                  Real time
-                                  <div class="btn-group" id="realtime" data-toggle="btn-toggle">
-                                    <button type="button" class="btn btn-default btn-xs active" data-toggle="on">On</button>
-                                    <button type="button" class="btn btn-default btn-xs" data-toggle="off">Off</button>
-                                  </div>
-                                </div> -->
+
                             </div>
                             <div class="box-body">
-                                <div id="switch2" style="height: 300px;"></div><!--interactive-->
-                            </div>
+								<div id="rootSwitchGraph" style="height: 100%; width:100%"></div> 
+							</div>
                             <!-- /.box-body-->
                         </div>
                         <!-- /.box -->
@@ -146,16 +240,10 @@ $users = $info['users'];
                                     </button>
                                 </div>
 
-                                <!-- <div class="box-tools pull-right">
-                                  Real time
-                                  <div class="btn-group" id="realtime" data-toggle="btn-toggle">
-                                    <button type="button" class="btn btn-default btn-xs active" data-toggle="on">On</button>
-                                    <button type="button" class="btn btn-default btn-xs" data-toggle="off">Off</button>
-                                  </div>
-                                </div> -->
+
                             </div>
                             <div class="box-body">
-                                <div id="switch1" style="height: 300px;"></div><!--interactive-->
+							<div id="slaveSwitchGraph" style="height: 100%; width:100%"></div> 
                             </div>
                         </div>
                             <!-- /.box-body-->
@@ -167,10 +255,13 @@ $users = $info['users'];
                 <!-- /.row -->
             </section>
             <!-- /.content -->
-        {{--</div>--}}
         <!-- /.content-wrapper -->
         </div>
 
     </div>
+    <!-- ./wrapper -->
+	<div>
+</body>
+</html>
 
 @endsection
