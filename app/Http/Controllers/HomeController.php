1777cc71a8de903633b541f9be9da0dafeb202d9
Ramya Payyavula <ramyaram48@gmail.com>
2018-11-06 00:31:56 -0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,9 +28,23 @@ class HomeController extends Controller
         $qvms=$home_wrapper->getAllQVMs();
         $servers=$home_wrapper->getAllServers();
         $users=$home_wrapper->getAllUsers();
+       $switches = $home_wrapper->getAllSwitches();
+        for($i=0;$i<count($switches);$i++){
+           if($switches[$i]['name']=='root-switch'){
+             $root_switch_id = $switches[$i]['switchID'];
+           }
+           else{
+             $slave_switch_id = $switches[$i]['switchID'];
+            }
+        }
+        $rootswitch_logs = $home_wrapper->getLogsBySwitch($root_switch_id);
+        $slaveswitch_logs = $home_wrapper->getLogsBySwitch($slave_switch_id);
         $info=array('qvms'=>count($qvms),
             'servers'=>count($servers),
-            'users'=>count($users));
+            'users'=>count($users),
+            'rootswitch_logs'=>$rootswitch_logs,
+            'slaveswitch_logs'=>$slaveswitch_logs);
+
 
         return view('home')->with('info', $info);
     }
