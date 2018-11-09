ddbed1a795dbca70c644c0433671c37aa720b432
Andrew Krall <ankwdf@mail.missouri.edu>
2018-11-09 16:44:10 -0600

    Fixing up controller installation script

--- setup_script/controller_install.sh
+++ setup_script/controller_install.sh
@@ -26,6 +26,10 @@ echo "# configured properly.                                         #"
 echo -e "################################################################"${NC}
 echo "" #Acts as a newline by outputting nothing.
 
+function checkErr() {
+    echo -e "${RED}$1 failed. Exiting.${NC}" >&2; exit;
+}
+
 #Check to see if the script is run as root/sudo. If not, warn the user and exit.
 if [[ $EUID -ne 0 ]] ; then
     echo -e "${RED}This script needs to be run as root. Please run this script again as root. Exiting.${NC}"
@@ -33,129 +37,81 @@ if [[ $EUID -ne 0 ]] ; then
 fi
 
 #Update the system's available list of packages and their versions.
-echo
-echo "Updating the system's available list of packages..."
-sudo apt-get -y update > /tmp/update_err.log 2>&1
+echo -e "${BLUE}Updating the system's available list of packages...${NC}"
+sudo apt-get -y update || checkErr "Package list installation"
 
-#Check to see if the package lists were updated properly.
-if [[ $? != 0 ]] ; then
-    echo -e "\t${RED}The package lists were not installed properly. The error was saved to /tmp/update_err.log. Exiting.${NC}"
-    exit
-else
-    #Remove the apache output log because it's not necessary.
-    echo -e "\t${GREEN}Packages updated properly!${NC}"
-    rm -f /tmp/update_err.log
-fi
+echo -e "${GREEN}Packages updated properly!${NC}"
+
 
 #Install the packages required for a LAMP stack, Python, Opam, and Mininet.
-echo "Installing LAMP stack, Python, Opam, and Mininet..."
-sudo apt-get -y install apache2 python-setuptools python-dev build-essential libcurl4-openssl-dev opam curl apt dpkg mininet m4 zlib1g-dev python-pip libmysqlclient-dev php libapache2-mod-php libssl-dev > /tmp/sw_install_err.log 2>&1
-
-#Check to see if the package lists were updated properly.
-if [[ $? != 0 ]] ; then
-    echo -e "\t${RED}The software was not installed properly. The error was saved to /tmp/sw_install_err.log. Exiting.${NC}"
-    exit
-else
-    #Remove the apache output log because it's not necessary.
-    echo -e "\t${GREEN}The required software was installed properly!${NC}"
-    rm -f /tmp/sw_install_err.log
-fi
-
-function installStatus() {
-    #Run command passed to installStatus()
-    "$@" > /tmp/install_err.log 2>&1 #Using this method instead of $* because $* will fail if any command arguments have spaces in them.
-    ret=$?
-    #Evaluate return value.
-    if [[ $ret != 0 ]] ; then
-        #Command did not execute properly.
-        echo -e "\t${RED}The command "@" did not execute properly. The error was saved to /tmp/install_err.log.${NC}"
-    else
-        #Command executed properly. Remove the error log file because it is no longer necessary.
-        rm -f /tmp/install_err.log
-    fi
-    echo $ret
-}
+echo -e "${BLUE}\nInstalling LAMP stack, Python, Opam, and Mininet...${NC}"
+sudo apt-get -y install apache2 python-setuptools python-dev build-essential libcurl4-openssl-dev opam curl apt dpkg mininet m4 zlib1g-dev python-pip libmysqlclient-dev php libapache2-mod-php libssl-dev || checkErr "Package installation"
+echo -e "${GREEN}The required software was installed properly!${NC}"
 
 #Configure Opam
 
-RET_ARR=()
+echo -e "${BLUE}Configuring Opam...${NC}"
+sudo opam init -y || checkErr "Opam configuration"
+sudo opam switch 4.06.0 || checkErr "Opam configuration"
+sudo opam switch || checkErr "Opam configuration"
+eval `opam config env` || checkErr "Opam configuration"
+sudo echo 'eval `opam config env`' >> ~/.profile || checkErr "Opam configuration"
 
-echo "Configuring Opam..."
-RET_ARR+=($(installStatus sudo opam init -y))
-RET_ARR+=($(installStatus sudo opam switch 4.06.0))
-RET_ARR+=($(installStatus sudo opam switch))
-installStatus eval `opam config env`
-installStatus sudo echo 'eval `opam config env`' >> ~/.profile
-
-for i in "${RET_ARR[@]}"; do
-    if [[ $i != 0 ]]; then
-        echo -e "\t${RED}Opam was not configured properly. Error was saved to /tmp/install_err.log. Exiting.${NC}"
-        exit;
-    fi
-done
-echo -e "\t${GREEN}Opam was configured properly.${NC}"
-
-R_ARR=()
+echo -e "${GREEN}Opam was configured properly.${NC}"
 
 #Install frenetic
-echo "Installing frenetic... (this might take a while)"
+echo -e "${BLUE}Installing frenetic... (this might take a while)${NC}"
 
-#Clear error file if exists.
-> /tmp/frenetic_err.log
+sudo opam pin add frenetic -y https://github.com/frenetic-lang/frenetic.git || checkErr "Frenetic installation"
+pip install pycurl frenetic mysql|| checkErr "Frenetic installation"
+sudo opam install frenetic || checkErr "Frenetic installation"
 
-sudo opam pin add frenetic -y https://github.com/frenetic-lang/frenetic.git >> /tmp/frenetic_err.log 2>&1
-
-#Save return values of commands run.
-R_ARR+=($?)
-
-pip install pycurl frenetic mysql >> /tmp/frenetic_err.log 2>&1
-
-R_ARR+=($?)
-
-sudo opam install frenetic >> /tmp/frenetic_err.log 2>&1
-
-R_ARR+=($?)
-
-# `pip install asdfasdf || \
-#  opam install`
-
-for i in "${R_ARR[@]}"; do
-    if [[ $i != 0 ]]; then
-        echo -e "\t${RED}Frenetic installation failed. Error was saved to /tmp/frenetic_err.log. Exiting.${NC}"
-        exit
-    fi
-done
-echo -e "\t${GREEN}Frenetic installation successful!${NC}"
-rm -f /tmp/frenetic_err.log
+echo -e "${GREEN}Frenetic installation successful!${NC}"
 
 #Create MySQL database
 
-sudo apt-get -y install debconf-utils
+echo -e "${BLUE}Installing MySQL database...${NC}"
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
+sudo apt-get -y install debconf-utils || checkErr "Debconf Utils Installation"
 
-sudo apt-get -y install mysql-server apache2 php libapache2-mod-php php-mysql php-curl
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password' || checkErr "Debconf configuration"
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password' || checkErr "Debconf configuration"
+sudo apt-get -y install mysql-server || checkErr "MySQL installation"
+
+sudo apt-get -y install mysql-server apache2 php libapache2-mod-php php-mysql php-curl || checkErr "MySQL installation"
+
+echo -e "${GREEN}MySQL installation complete!"
+
+#Configure MySQL database
+echo -e "${BLUE}Configuring MySQL database${NC}"
+
 cd /etc/mysql/mysql.conf.d 
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/g" mysqld.cnf
 
-#Configure MySQL database
 mysql -u root -ppassword << DB_SCRIPT
 CREATE DATABASE lab2db;
 CREATE USER 'monty'@'%' IDENTIFIED BY 'some_pass'; 
 GRANT ALL PRIVILEGES ON *.* TO 'monty'@'%';
 DB_SCRIPT
 
-echo "Configuring MySQL"
+sudo ufw allow 3306 || checkErr "MySQL configuration"
 
-sudo ufw allow 3306
+sudo service mysql restart || checkErr "MySQL configuration"
 
-sudo service mysql restart
+echo -e "${GREEN}MySQL configuration complete!"
 
-sudo apt-get -y install dnsutils
+echo -e "${BLUE}\nInstalling DNSUtils package to obtain the controller's public IP address...${NC}"
+
+sudo apt-get -y install dnsutils || checkErr "DNSUtils package"
 
 publicIpAddr=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+if [ -z "$publicIpAddr" ] ; then
+    echo "${RED}The controller public IP address could not be obtained. Exiting.${NC}"
+    exit
+fi
+
+echo -e "${GREEN}The controller's public IP address was obtained successfully!${NC}"
 
 echo "The configuration for the controller has been completed!"
 echo "To log in to the database on any of the switches, use:"
