b98cbcdc2b3516ddd1020ba106f8fcb201545dbb
Ramya Payyavula <ramyaram48@gmail.com>
2018-11-14 21:55:38 -0600

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,6 +24,12 @@
                                     @endif
                                 </div>
                             </div>
+                            <div class="form-group">
+                                <label for="email" class="col-md-4 control-label">Email ID</label>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                </div>
+                            </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <label for="password" class="col-md-4 control-label">Password</label>
