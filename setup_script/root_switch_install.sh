05612b9fdcc7b0a8cb3b336d42307f9d6a9dfd62
Andrew Krall <ankwdf@mail.missouri.edu>
2018-11-27 16:46:00 -0600

    Update root_switch_install.sh

--- setup_script/root_switch_install.sh
+++ setup_script/root_switch_install.sh
@@ -112,9 +112,11 @@ sudo ovs-vsctl add-br br0
 sudo ifconfig eth1 0
 sudo ifconfig eth2 0
 sudo ifconfig eth3 0
+sudo ifconfig eth4 0
 sudo ovs-vsctl add-port br0 eth1
 sudo ovs-vsctl add-port br0 eth2
 sudo ovs-vsctl add-port br0 eth3
+sudo ovs-vsctl add-port br0 eth4
 
 #Configure the controller on bridge b0 for the root switch.
 sudo ovs-vsctl set-controller br0 tcp:$ipAddress:6633 || checkErr "Networking configuration"
